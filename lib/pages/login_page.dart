1d8e5f99e97512984b830cb7d9c031881b642e14
DrMrJanko <91409403+DrMrJanko@users.noreply.github.com>
2022-03-26 10:19:14 +0100

    Added files for prototype
    Add_product_page
    Login_page
    Product_page (for user and moderator)
    Register_page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,9 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:sara/classes/singleton.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key, required this.title}) : super(key: key);
+  LoginPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -13,6 +14,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String password = "";
+  String username = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,27 +39,36 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
-                  padding: EdgeInsets.only(top: 5, bottom: 5),
-                  child: Container(
-                      width: 150,
-                      child: Text("Uloguj se",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.italic,
-                          )))),
+                padding: const EdgeInsets.only(top: 5, bottom: 5),
+                child: Container(
+                  width: 150,
+                  child: const Text(
+                    "Uloguj se",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+              ),
               Padding(
-                  padding: EdgeInsets.only(top: 5, bottom: 5),
+                  padding: const EdgeInsets.only(top: 5, bottom: 5),
                   child: Container(
                     width: 150,
                     decoration: BoxDecoration(
                       border:
-                          Border.all(color: Color.fromARGB(255, 140, 187, 241)),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                          Border.all(color: const Color.fromARGB(255, 140, 187, 241)),
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
                     ),
                     child: TextField(
-                      decoration: InputDecoration(hintText: "Korisnicko ime"),
+                      decoration: const InputDecoration(hintText: "Korisnicko ime"),
+                      onChanged: (text) {
+                        setState(() {
+                          username = text;
+                        });
+                      },
                     ),
                   )),
               Padding(
@@ -71,24 +83,71 @@ class _LoginPageState extends State<LoginPage> {
                     child: TextField(
                       decoration: InputDecoration(hintText: "Lozinka"),
                       obscureText: true,
+                      onChanged: (text) {
+                        setState(() {
+                          password = text;
+                        });
+                      },
                     ),
                   )),
               SizedBox(
-                  width: 150,
-                  child: Padding(
-                      padding: EdgeInsets.only(top: 5, bottom: 5),
-                      child: TextButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Color.fromARGB(255, 140, 187, 241))),
-                        onPressed: () {},
-                        child: Text(
-                          "Uloguj se",
-                          style: TextStyle(
-                            color: (Colors.white),
+                width: 150,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 5, bottom: 5),
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromARGB(255, 140, 187, 241))),
+                    onPressed: () {
+                      if (username == password && username == "admin") {
+                        Singleton().mod = "A";
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text(
+                            "Ulogovan kao ADMIN",
+                            textAlign: TextAlign.center,
                           ),
-                        ),
-                      ))),
+                        ));
+                        Navigator.pop(context);
+                      } else if (username == password && username == "mod") {
+                        Singleton().mod = "M";
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text(
+                            "Ulogovan kao MODERATOR",
+                            textAlign: TextAlign.center,
+                          ),
+                        ));
+                        Navigator.pop(context);
+                      } else if (username == password && username == "user") {
+                        Singleton().mod = "U";
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text(
+                            "Ulogovan kao registrovani korisnik",
+                            textAlign: TextAlign.center,
+                          ),
+                        ));
+                        Navigator.pop(context);
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text(
+                            "Netacna sifra ili korisnicko ime",
+                            textAlign: TextAlign.center,
+                          ),
+                        ));
+                      }
+                    },
+                    child: const Text(
+                      "Uloguj se",
+                      style: TextStyle(
+                        color: (Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
               Container(
                   width: 150,
                   child: ClipRRect(
@@ -96,7 +155,29 @@ class _LoginPageState extends State<LoginPage> {
                       child: SignInButton(
                         Buttons.Google,
                         text: " Uloguj se",
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(
+                            () {
+                              if (username == password && username == "admin") {
+                                Singleton().mod = "A";
+                              } else if (username == password &&
+                                  username == "mod") {
+                                Singleton().mod = "M";
+                              } else if (username == password &&
+                                  username == "user") {
+                                Singleton().mod = "U";
+                              } else {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(const SnackBar(
+                                  content: Text(
+                                    "Netacna sifra ili korisnicko ime",
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ));
+                              }
+                            },
+                          );
+                        },
                       )))
             ],
           ),
---
+++ lib/pages/mod_product_page.dart
@@ -0,0 +1,175 @@
+//Author: Janko Tufegdzic 0097/19
+
+import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
+import 'package:sara/classes/recenzija.dart';
+
+class ModProductPage extends StatefulWidget {
+  const ModProductPage({Key? key}) : super(key: key);
+
+  @override
+  State<ModProductPage> createState() => _ModProductPageState();
+}
+
+class _ModProductPageState extends State<ModProductPage> {
+  bool vrednost = false;
+  var recenzija =
+      Recenzija(korisnik: "Pavle", recenzija: "Jako udobna kosulja.");
+  var rec1 = Recenzija(korisnik: "Dusan", recenzija: "Prijatan materijal.");
+  var rec2 = Recenzija(korisnik: "Stefan", recenzija: "Lepa kosulja.");
+  var rec3 =
+      Recenzija(korisnik: "Marko", recenzija: "Osecam se prijatno u njoj.");
+  String size = "S";
+
+  var sizes = ['S', 'M', 'L', 'XL', 'XXL'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 140, 187, 241),
+          toolbarHeight: 100,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                "assets/logoWhite.png",
+                fit: BoxFit.contain,
+                height: 100,
+              ),
+            ],
+          ),
+          centerTitle: true,
+        ),
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          return Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            SingleChildScrollView(
+              child: Container(
+                  width: constraints.maxWidth / 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/slika1.png",
+                      ),
+                      Image.asset(
+                        "assets/slika2.png",
+                      ),
+                      Image.asset(
+                        "assets/slika3.png",
+                      ),
+                    ],
+                  )),
+            ),
+            Container(
+                width: constraints.maxWidth / 2,
+                child: Column(children: [
+                  Container(
+                      height: constraints.maxHeight / 2,
+                      child: Column(children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Crna kosulja",
+                                style: TextStyle(
+                                    fontSize: 48,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 5,
+                                    color: Color.fromARGB(255, 140, 187, 241)),
+                              ),
+                            ]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "2300 dinara",
+                                style: TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 140, 187, 241)),
+                              ),
+                            ]),
+                        Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(4))),
+                                    child: SizedBox(
+                                      width: 270,
+                                      child: DropdownButton(
+                                          value: size,
+                                          icon: Row(children: [
+                                            Text("  Izaberite velicinu"),
+                                            Icon(Icons.arrow_drop_down)
+                                          ]),
+                                          dropdownColor: Color.fromARGB(
+                                              255, 140, 187, 241),
+                                          items: sizes.map((String sizes) {
+                                            return DropdownMenuItem(
+                                                child: Text(sizes),
+                                                value: sizes);
+                                          }).toList(),
+                                          onChanged: (String? newVal) {
+                                            setState(() {
+                                              size = newVal!;
+                                            });
+                                          }),
+                                    )),
+                                Text(" "),
+                              ],
+                            )),
+                        Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SizedBox(
+                                    height: 70,
+                                    width: 200,
+                                    child: TextButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          "Dodaj u korpu",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  Color.fromARGB(
+                                                      255, 140, 187, 241)),
+                                        ))),
+                                LikeButton(size: 70)
+                              ],
+                            )),
+                        Text("")
+                      ])),
+                  Text(
+                    "Recenzije",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 140, 187, 241),
+                        fontSize: 35,
+                        fontStyle: FontStyle.italic),
+                  ),
+                  Container(
+                      height: constraints.maxHeight / 3,
+                      child: SingleChildScrollView(
+                        child: Column(children: [
+                          recenzija.format(),
+                          rec1.format(),
+                          rec2.format(),
+                          rec3.format()
+                        ]),
+                      ))
+                ]))
+          ]);
+        }));
+  }
+}
