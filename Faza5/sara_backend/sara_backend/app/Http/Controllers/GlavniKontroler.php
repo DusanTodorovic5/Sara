56bcb32d86a250a0439f5e1cae93a87d8c4b9ef9
Ana Maksimovic <ma190174d@student.etf.bg.ac.rs>
2022-05-23 16:40:53 +0200

    izmena login_submit f-je

--- Faza5/sara_backend/sara_backend/app/Http/Controllers/GlavniKontroler.php
+++ Faza5/sara_backend/sara_backend/app/Http/Controllers/GlavniKontroler.php
@@ -14,10 +14,12 @@ use Illuminate\Support\Facades\Auth;
  * GlavniKontroler je klasa izvedena iz Controller klase, zaduzena za opstu kontrolu sistema
  * Metod je zaduzen za registraciju novih korisnika 
  */
+
 class GlavniKontroler extends Controller
 {
     // Metod register_user prima Request iz post zahteva i vraca status o uspesnosti registracije
-    function register_user(Request $request){
+    function register_user(Request $request)
+    {
         // Metod json()->all() vraca listu parametara json objekta koji je poslat kao telo zahteva
         $body = $request->json()->all();
         /*
@@ -27,26 +29,19 @@ class GlavniKontroler extends Controller
         return Odobravanje::add_new($body);
     }
 
-     /* 
+    /* 
     * funkcija koja vraca sve podatke o korisniku 
     * ukoliko je logovanje bilo uspesno, u suprotnom
     * vraca false
     */
-    public function login_submit(Request $request){
-        $this->validate(
-            $request,[
-                'Username' => 'required',
-                'Password' => 'required'
-            ],[
-                'required' => 'Polje :attribute je obavezno'
-            ]
-        );
+    public function login_submit(Request $request)
+    {
 
-        if (!auth()->attempt($request->only('Username','Password'))){       
-               return response()->json([
-                'success'=>false,
-            ]);           
-         } 
+        if (!auth()->attempt($request->only('Username', 'Password'))) {
+            return response()->json([
+                'success' => false,
+            ]);
+        }
 
         return auth()->user();
     }
@@ -57,13 +52,13 @@ class GlavniKontroler extends Controller
     public function logout()
     {
         auth()->logout();
-        
     }
     /*
      * Metod filtera koji se prosledjuje proizvodu, prima kao argument zahtev 
      * i prosledjuje telo zahteva proizvodu
-     */ 
-    public function filter(Request $request) {
+     */
+    public function filter(Request $request)
+    {
         return Proizvod::filter($request->json()->all());
     }
     /*
@@ -74,25 +69,26 @@ class GlavniKontroler extends Controller
      * ova metoda filtrira proizvode po njegovom polu i uzrastu
      * 
      * U slucaju da metoda ne nadje odgovarajuce proizvode, vraca nasumicnih 6 elemenata iz tabele proizvoda
-     */ 
-    public function preporuka($lat, $long) {
+     */
+    public function preporuka($lat, $long)
+    {
         // Dohvata vreme sa API-ka
         $vreme = WeatherApi::get_weather($lat, $long);
         // Generise godisnje doba u zavisnosti od vremenskih uslova
         $doba = "Prolece";
         if (($vreme[0] == "Mestimicno Oblacno" || $vreme[0] == "Vedro") &&
-            $vreme[1] == "Suvo" && $vreme[2] == "Toplo") {
+            $vreme[1] == "Suvo" && $vreme[2] == "Toplo"
+        ) {
             $doba = "Leto";
-        }
-        else if (($vreme[0] == "Mestimicno Oblacno" || $vreme[0] == "Vedro") &&
-                ($vreme[1] == "Suvo") && $vreme[2] == "Normalno") {
+        } else if (($vreme[0] == "Mestimicno Oblacno" || $vreme[0] == "Vedro") &&
+            ($vreme[1] == "Suvo") && $vreme[2] == "Normalno"
+        ) {
             $doba = "Prolece";
-        } 
-        else if (($vreme[0] == "Mestimicno Oblacno" || $vreme[0] == "Oblacno") &&
-                ($vreme[1] == "Pljusak" || $vreme[1] == "Kisa") && $vreme[2] == "Normalno") {
+        } else if (($vreme[0] == "Mestimicno Oblacno" || $vreme[0] == "Oblacno") &&
+            ($vreme[1] == "Pljusak" || $vreme[1] == "Kisa") && $vreme[2] == "Normalno"
+        ) {
             $doba = "Jesen";
-        }
-        else if (($vreme[0] == "Mestimicno Oblacno") || ($vreme[0] == "Oblacno") && $vreme[2] == "Hladno") {
+        } else if (($vreme[0] == "Mestimicno Oblacno") || ($vreme[0] == "Oblacno") && $vreme[2] == "Hladno") {
             $doba = "Zima";
         }
         /*
@@ -100,27 +96,27 @@ class GlavniKontroler extends Controller
          * Ne treba pomesati sa Sezona, koja moze biti Leto 2022 ili Leto 2021 i 
          * oznacava da je tada izasla kolekcija a ne da se nosi u tom godisnjem dobu
          */
-        $query = Proizvod::whereRaw('Tagovi like "%'.$doba.'%"');
+        $query = Proizvod::whereRaw('Tagovi like "%' . $doba . '%"');
         // Ako je korisnik prijavljen, filtrirace po njegovom kriterijumu
-        if (Auth::check()){
+        if (Auth::check()) {
             $kor = Korisnik::dohv_sa_id(Auth::user()->id);
 
             $godine = $kor->dohvati_godine();
-            
-            $query = $query->whereRaw('godinaOd <= '.$godine.' AND godinaDo >='.$godine);
+
+            $query = $query->whereRaw('godinaOd <= ' . $godine . ' AND godinaDo >=' . $godine);
             $pol = 'M';
-            if ($kor->Pol == 'M'){
+            if ($kor->Pol == 'M') {
                 $pol = 'Z';
             }
-            $query = $query->where('Pol','<>',$pol);
+            $query = $query->where('Pol', '<>', $pol);
         }
         // Dohvata upit
         $result = $query->get();
         // Ako je upit prazan, nacice 6 nasumicnih proizvoda
-        if (count($result) < 1){
+        if (count($result) < 1) {
             $result = Proizvod::all()->array;
             shuffle($result);
-            $result = array_slice($result,0,6);
+            $result = array_slice($result, 0, 6);
         }
         // Vraca json niz preporucenih proizvoda
         return $result;
