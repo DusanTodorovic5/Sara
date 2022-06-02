5bc603be9f3486769478057c2a3d585035e65634
anamaksimovic <aanamaksimovic9@gmail.com>
2022-06-02 20:46:36 +0200

    dodati komentari za ListaZelja

--- Faza5/sara_backend/sara_backend/app/Models/ListaZelja.php
+++ Faza5/sara_backend/sara_backend/app/Models/ListaZelja.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Klasa Komentar predstavlja model Komentara iz baze podataka.
+ * Klasa ListaZelja predstavlja model Zeli iz baze podataka.
  */
 class ListaZelja extends Model
 {
@@ -20,24 +20,35 @@ class ListaZelja extends Model
     ];
 
     /*
-    * funkcija koja dohvata komentar
-    * sa zadatim id
+    * funkcija koja dohvata listu zelja 
+    * za zadatog korisnika
     */
     public static function dohvati_sa_idKorisnika($id)
     {
         return ListaZelja::where('IdKorisnika', $id)->get();
     }
 
+    /*
+    * funkcija koja dohvata red u tabeli zeli
+    * sa zadatim idKorisnik i idProizvod
+    */
     public static function dohv($idProizvod, $idKorisnik)
     {
         return ListaZelja::where(['IdKorisnika' => $idKorisnik, 'IdProizvoda' => $idProizvod])->get();
     }
 
+    /*
+    * funkcija koja menja polje za obavestavanje o dostupnosti
+    */
     public static function azuriraj_polje($idProizvod, $idKorisnik, $polje)
     {
         return ListaZelja::where(['IdKorisnika' => $idKorisnik, 'IdProizvoda' => $idProizvod])->update(['Obavesti' => $polje]);
     }
 
+    /*
+    * funkcija koja brise red u tabeli zeli
+    * sa zadatim idKorisnik i idProizvod
+    */
     public static function obrisi($idProizvod, $idKorisnik)
     {
         return ListaZelja::where('IdKorisnika', $idKorisnik)->where('IdProizvoda', $idProizvod)->delete();
